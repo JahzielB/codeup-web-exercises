a20c38b4b290fb70be303c4a6baa1920cac95d80
JahzielB <jahzielb@gmail.com>
2020-04-17 16:54:50 -0500

    created a js directory and did most of the javascript with html exercise

---
+++ js/external.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello from inline JavaScript")
+alert("Welcome to my Website!");
+var userInput = prompt('What is your favorite color?:');
+alert("My favorite color is also " + userInput + "!");
