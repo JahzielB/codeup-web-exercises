31abd53032aa27107925e1b01962ce285f6fc73e
JahzielB <jahzielb@gmail.com>
2020-04-20 11:05:42 -0500

    changed all of the word problems from a fixed value to instead take user input

--- js/external.js
+++ js/external.js
@@ -5,40 +5,40 @@ var userInput = prompt('What is your favorite color?:');
 alert("My favorite color is also " + userInput + "!");
 
 // Word Problem #1 from Working With Data Types, Operators, and Variables
-var littleMermaid = 3;
-var brotherBear = 5;
-var hercules = 1;
-var moviePricePerDay = 3;
-var totalCost = (littleMermaid + brotherBear + hercules) * 3;
-alert("The total cost of the movies is $" + totalCost + ".");
+var littleMermaid = parseInt(prompt("How many days will The little Mermaid be rented?"));
+var brotherBear = parseInt(prompt("How many days will Brother Bear be rented?"));
+var hercules = parseInt(prompt("How many days will Hercules be rented?"));
+var moviePricePerDay = parseInt(prompt("What is the daily rental rate for movies today?"));
+var totalCost = (littleMermaid + brotherBear + hercules) * moviePricePerDay;
+alert("The total cost of the movies is $" + totalCost.toFixed(2) + ".");
 
 // Word Problem #2 from Working With Data Types, Operators, and Variables
-var googlePay = 400;
-var amazonPay = 380;
-var facebookPay = 350;
-var googleHours = 6;
-var amazonHours = 4;
-var facebookHours = 10;
+var googlePay = parseInt(prompt("How much is Google paying you by the hour?"));
+var amazonPay = parseInt(prompt("How much is Amazon paying you by the hour?"));
+var facebookPay = parseInt(prompt("How much is Facebook paying you by the hour?"));
+var googleHours = parseInt(prompt("How many hours did you work for Google?"));
+var amazonHours = parseInt(prompt("How many hours did you work for Amazon?"));
+var facebookHours = parseInt(prompt("How many hours did you work for Facebook?"));
 var payThisWeek = (googlePay * googleHours) + (amazonPay * amazonHours) + (facebookPay * facebookHours);
-alert("The pay I will be receiving this week amounts to $" + payThisWeek + ".");
+alert("The pay you will be receiving this week amounts to $" + payThisWeek.toFixed(2) + ".");
 
 // Word Problem #3 from Working With Data Types, Operators, and Variables
-var classNotFull = true;
-var noScheduleConflict = true;
+var classNotFull = confirm("Is the class full?");
+var noScheduleConflict = confirm("Does this class conflict with your schedule?");
 var studentEnroll = classNotFull && noScheduleConflict;
 function canStudentEnroll() {
     if (studentEnroll === true) {
-        return alert("This class is not full and does not conflict with your schedule.");
+        return alert("You may enroll in this class.");
     } else {
-        return alert("This class is full and/or conflicts with your schedule.");
+        return alert("You may not enroll in this class.");
     }
 }
 canStudentEnroll();
 
 // Word Problem #4
 var itemsPurchased = parseInt(prompt("How many items have you purchased?"), 10);
-var offerNotExpired = true;
-var premiumMember = false;
+var offerNotExpired = confirm("Has the offer not expired?");
+var premiumMember = confirm("Are you a premium member?");
 var productOffer = ((itemsPurchased > 2) || premiumMember) && offerNotExpired;
 function doesOfferApply() {
     if (productOffer === true) {
