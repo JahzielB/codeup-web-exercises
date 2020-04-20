ed1b2fc58f5f43665863385eedb33eed9222ada8
JahzielB <jahzielb@gmail.com>
2020-04-20 13:59:25 -0500

    completed js mini function problems

--- js/external.js
+++ js/external.js
@@ -5,20 +5,20 @@ var userInput = prompt('What is your favorite color?:');
 alert("My favorite color is also " + userInput + "!");
 
 // Word Problem #1 from Working With Data Types, Operators, and Variables
-var littleMermaid = parseInt(prompt("How many days will The little Mermaid be rented?"));
-var brotherBear = parseInt(prompt("How many days will Brother Bear be rented?"));
-var hercules = parseInt(prompt("How many days will Hercules be rented?"));
-var moviePricePerDay = parseInt(prompt("What is the daily rental rate for movies today?"));
+var littleMermaid = Number(prompt("How many days will The little Mermaid be rented?"));
+var brotherBear = Number(prompt("How many days will Brother Bear be rented?"));
+var hercules = Number(prompt("How many days will Hercules be rented?"));
+var moviePricePerDay = Number(prompt("What is the daily rental rate for movies today?"));
 var totalCost = (littleMermaid + brotherBear + hercules) * moviePricePerDay;
 alert("The total cost of the movies is $" + totalCost.toFixed(2) + ".");
 
 // Word Problem #2 from Working With Data Types, Operators, and Variables
-var googlePay = parseInt(prompt("How much is Google paying you by the hour?"));
-var amazonPay = parseInt(prompt("How much is Amazon paying you by the hour?"));
-var facebookPay = parseInt(prompt("How much is Facebook paying you by the hour?"));
-var googleHours = parseInt(prompt("How many hours did you work for Google?"));
-var amazonHours = parseInt(prompt("How many hours did you work for Amazon?"));
-var facebookHours = parseInt(prompt("How many hours did you work for Facebook?"));
+var googlePay = Number(prompt("How much is Google paying you by the hour?"));
+var amazonPay = Number(prompt("How much is Amazon paying you by the hour?"));
+var facebookPay = Number(prompt("How much is Facebook paying you by the hour?"));
+var googleHours = Number(prompt("How many hours did you work for Google?"));
+var amazonHours = Number(prompt("How many hours did you work for Amazon?"));
+var facebookHours = Number(prompt("How many hours did you work for Facebook?"));
 var payThisWeek = (googlePay * googleHours) + (amazonPay * amazonHours) + (facebookPay * facebookHours);
 alert("The pay you will be receiving this week amounts to $" + payThisWeek.toFixed(2) + ".");
 
@@ -36,7 +36,7 @@ function canStudentEnroll() {
 canStudentEnroll();
 
 // Word Problem #4
-var itemsPurchased = parseInt(prompt("How many items have you purchased?"), 10);
+var itemsPurchased = Number(prompt("How many items have you purchased?"), 10);
 var offerNotExpired = confirm("Has the offer not expired?");
 var premiumMember = confirm("Are you a premium member?");
 var productOffer = ((itemsPurchased > 2) || premiumMember) && offerNotExpired;
---
+++ js/mini-functions-problems.js
@@ -0,0 +1,33 @@
+"use strict";
+// ===== MINI PROBLEMS
+// Write a function, returnFive, that returns the number five. No inputs should be defined.
+function returnFive() {
+    return 5;
+}
+
+// Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+function isFive(num) {
+    if (num == 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+function isShortWord(words) {
+    return words.length < 5;
+}
+
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+function isSameLength(str1, str2) {
+    return str1.length === str2.length;
+}
+
+// Write a function, getSmallerSegment, that takes in a string and a number input. The function should return a substring of the first argument that is as many characters long as the second argument in lowercase.
+// example input: getSmallerSegment("Codeup", 3)
+// example output: "cod"
+
+function getSmallerSegment(str1, num1) {
+    return str1.substring(0, num1).toLowerCase()
+}
