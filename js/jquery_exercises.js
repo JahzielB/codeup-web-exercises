1f9c23da60902dbf5871b0f2bf389cb71fafe28f
JahzielB <jahzielb@gmail.com>
2020-05-13 10:54:30 -0500

    finished jquery mouse events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,7 +2,7 @@
 
 (function($){
     $(document).ready(function() {
-        alert("The DOM has finished loading!");
+        // alert("The DOM has finished loading!");
 
         // var firstH1 = $("#first-h1").html();
         // var firstH1 = $("#first-li").html();
@@ -10,7 +10,22 @@
 
         // $(".codeup").css("border", "2px solid black");
 
-        $("li").css("font-size", "20px");
-        $("h1, p, li").css("background-color", "yellow");
+        // $("li").css("font-size", "20px");
+        // $("h1, p, li").css("background-color", "yellow");
+
+        $("#first-h1").click(function () {
+            $(this).css("background-color", "blue");
+        });
+        $(".paragraph").dblclick(function () {
+            $(this).css("font-size", "18px");
+        });
+        $("li").hover(
+            function () {
+                $(this).css("color", "red")
+            },
+            function () {
+                $(this).css("color", "black")
+            }
+        );
     });
 })(jQuery);
