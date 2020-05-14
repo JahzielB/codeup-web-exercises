52e277c456e11c52c7fddb48595f64185679beea
JahzielB <jahzielb@gmail.com>
2020-05-14 17:01:27 -0500

    completed second exercise for essential methods

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,14 +1,32 @@
 "use strict";
 (function ($) {
     $(document).ready(function () {
-        $("dl").after("<button type='button' id='secrets'>Reveal Secrets</button>")
+
+        $("dd").addClass("invisible");
         $("#secrets").click(function () {
-            // $(".invisible").css("visibility", "visible");
             $("dd").toggleClass("invisible");
             $(this).text($(this).text() === 'Reveal Secrets' ? 'Hide Secrets' : 'Reveal Secrets')
         })
         $("dt").click(function () {
            $(this).toggleClass("highlight");
         });
-    })
+
+        $("#more-secrets").click(function () {
+            $("ul").each(function(){
+                $(this).children().last().toggleClass("highlight");
+            });
+        });
+
+        $("h3").click(function () {
+            $(this).next().each(function (){
+                $(this).css("font-weight", "bold");
+            });
+        });
+
+        $("li").click(function () {
+            $(this).parent().each(function(){
+                $(this).children().first().css("color", "blue");
+            });
+        });
+    });
 })(jQuery)
