bab2838951ce6ab38b4f5f280ea346ba5ed080ab
JahzielB <jahzielb@gmail.com>
2020-05-26 11:19:48 -0500

    fixed promise exercise problems

--- js/promises.js
+++ js/promises.js
@@ -4,31 +4,38 @@
     // let githubAPI = fetch("https://api.github.com/users/JahzielB/events", {headers: {'Authorization': `token ${GITHUB_KEY}`}})
     //
     // githubAPI.then(response => response.json())
-    //     // .then(jsonResponse => console.log(jsonResponse.filter(user => user.type === "PushEvent")[0].created_at))
+    //     .then(jsonResponse => console.log(jsonResponse.filter(user => user.type === "PushEvent")[0].created_at))
     //     .then(jsonResponse => console.log(jsonResponse))
     //     .catch(error => console.log(error))
 
     let lastCommit = (username, githubKey) => {
-        let githubAPI = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${githubKey}`}})
-
-        githubAPI.then(response => response.json())
-            .then(jsonResponse => console.log(jsonResponse.filter(user => user.type === "PushEvent")[0].created_at))
+        return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${githubKey}`}})
+            .then(response => response.json())
+            .then(jsonResponse => jsonResponse.filter(user => user.type === "PushEvent")[0].created_at)
+            .then(date => new Date(date).toDateString())
             .catch(error => console.log(error))
     }
 
-    // lastCommit("JahzielB", GITHUB_KEY);
+    lastCommit("JahzielB", GITHUB_KEY).then(data => console.log(data))
 
     let wait = num => {
-        let milliseconds = num * 1000;
+        let milliseconds = num / 1000;
         return new Promise((resolve) => {
             setTimeout(() => {
-                resolve();
-            }, milliseconds);
+                resolve(`You'll see this after ${milliseconds} seconds`);
+            }, num);
         });
     }
 
-    // console.log(wait(2));
+    wait(3000).then((data) => console.log(data));
 
-    wait(3).then(() => console.log("You'll see this after 3 seconds"));
+    // let ganymedeGithubUsernames = ["aaronsingleterry10", "AndrewBrought", "armandoroman20", "AugustineCervantesIII", "bmarchambault", "chrise08", "Damontheath", "DylanLeifeste", "Fernando5712", "george412williams", "gregjett92", "JahzielB", "jayarredondo", "juliacontreras483", "JustinMcCaleb", "cottoknoah", "larrycastillo2009", "lauraldavis", "lgrant146", "MarkTAlonge", "mgarrisonn", "ramon-villarreal-leal", "Shelbypol", "trevormullis12"];
+    //
+    // ganymedeGithubUsernames.forEach(username => {
+    //     fetch(`https://api.github.com/repos/${username}/codeup-web-exercises/contents/js/keys.js`, {headers: {'Authorization': `token ${GITHUB_KEY}`}})
+    //         .then(response => response.json())
+    //         .then(jsonResponse => console.log(jsonResponse))
+    //         .catch(error => console.log(error))
+    // });
 
 })();
